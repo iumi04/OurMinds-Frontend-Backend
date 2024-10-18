1832108ab4f907b74da290a6512461767ac9241d
iumi04 <pg13ftw@gmail.com>
2024-10-18 17:28:36 -0400

    dont have an account yet? Sign up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,44 +4,52 @@ import apiService from '../../services/apiService';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login({ setToken }) {
-    const [username, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        setError('');
-        try {
-            const token = await apiService.login({ username, password });
-            localStorage.setItem('token', token);
-            setToken(token);
-            navigate('/today');
-        } catch (err) {
-            setError('Invalid username or password');
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const token = await apiService.login({ username, password });
+      localStorage.setItem("token", token);
+      setToken(token);
+      navigate("/today");
+    } catch (err) {
+      setError("Invalid username or password");
     }
+  };
 
-    return (
-      <div className="login-wrapper">
-        <h1>Please Log In🙏</h1>
-        {error && <p className="error">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <label>
-            <p>Username</p>
-            <input type="text" onChange={(e) => setUserName(e.target.value)} />
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    );
+  return (
+    <div className="login-wrapper">
+      <h1>Please Log In🙏</h1>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div className="signup-link">
+          <p>
+            Don't have an account yet?{" "}
+            <span onClick={() => navigate("/register")} className="link">
+              Sign up
+            </span>
+          </p>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
