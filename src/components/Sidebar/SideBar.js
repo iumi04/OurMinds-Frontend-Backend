802c60ce3683c709ec0e1718d2d986d39f7ea663
iumi04 <pg13ftw@gmail.com>
2024-10-18 16:27:06 -0400

    other pages not accessible (visually)

--- src/components/Sidebar/SideBar.js
+++ src/components/Sidebar/SideBar.js
@@ -16,27 +16,27 @@ const Sidebar = () => {
     };
 
     return (
-        <div className="sidebar">
-            <div className='side-bar-logo'>
-                <div className='log-img'>
-                    <img src={Logo} alt='logo' />
-                </div>
-                <div>
-                    <h3 className=' mt-3'>OurMinds Journaling</h3>
-                </div>
-            </div>
-            <ul>
-
-                <li
-                    className={selectedItem === 'today' ? 'selected' : ''}
-                    onClick={() => handleItemClick('today')}
-                >
-                    <Link to="/">
-                        <img src={Today} alt='today' width={22} height={22} />
-                        <span className='px-2'>Today</span>
-                    </Link>
-                </li>
-                <li
+      <div className="sidebar">
+        <div className="side-bar-logo">
+          <div className="log-img">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div>
+            <h3 className=" mt-3">OurMinds Journaling</h3>
+          </div>
+        </div>
+        <ul>
+          <li
+            className={selectedItem === "today" ? "selected" : ""}
+            onClick={() => handleItemClick("today")}
+          >
+            <Link to="/">
+              <img src={Today} alt="today" width={22} height={22} />
+              <span className="px-2">Today</span>
+            </Link>
+          </li>
+          {/*BRING BACK LATER*/}
+          {/* <li
                     className={selectedItem === 'calendar' ? 'selected' : ''}
                     onClick={() => handleItemClick('calendar')}
                 >
@@ -53,23 +53,23 @@ const Sidebar = () => {
                         <img src={Prompts} alt='prompts' width={22} height={22} />
                         <span className='px-2'>Prompts</span>
                     </Link>
-                </li>
-            </ul>
-            <ul className='last-element'>
-                <li
-                    className={selectedItem === 'Avtar' ? 'selected' : ''}
-                    onClick={() => handleItemClick('Avtar')}
-                >
-                    <Link to="/prompt">
-                        <img src={Avtar} alt='prompts' width={30} height={30} />
-                        <span className='px-2'>Ophelia W.</span>
-                        <div className='px-5'>
-                            <img src={Arrow} alt='Arrow' width={25} height={25} />
-                        </div>
-                    </Link>
-                </li>
-            </ul>
-        </div>
+                </li> */}
+        </ul>
+        <ul className="last-element">
+          <li
+            className={selectedItem === "Avtar" ? "selected" : ""}
+            onClick={() => handleItemClick("Avtar")}
+          >
+            <Link to="/prompt">
+              <img src={Avtar} alt="prompts" width={30} height={30} />
+              <span className="px-2">Ophelia W.</span>
+              <div className="px-5">
+                <img src={Arrow} alt="Arrow" width={25} height={25} />
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
     );
 };
 
